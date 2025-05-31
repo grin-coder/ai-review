53b44fe0f3259197fd001833607f4955c2f8fb44
旺看天 <2417266532@qq.com>
2025-05-31 21:08:31 +0800

    feat:工程初次提交

---
+++ review-sdk/src/main/java/com/grin/Main.java
@@ -0,0 +1,16 @@
+package com.grin;
+
+/**
+ * ClassName:${NAME}
+ * Package:com.grin
+ * Description
+ *
+ * @Author :刘昂
+ * @Create :${DATE}--${TIME}
+ * @Version :v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
