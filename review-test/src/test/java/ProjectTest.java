cf4a6772b5c4c5d8575918a486f2ed93c85fb8c2
旺看天 <2417266532@qq.com>
2025-06-01 10:43:25 +0800

    feat:添加测试类

---
+++ review-test/src/test/java/ProjectTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * ClassName:ProjectTest
+ * Package:PACKAGE_NAME
+ * Description
+ *
+ * @Author :刘昂
+ * @Create :2025/6/1--10:41
+ * @Version :v1.0
+ */
+@SpringBootTest
+@RunWith(JUnit4.class)
+public class ProjectTest {
+    @Test
+    public void testFirst(){
+        System.out.println("aaa");
+    }
+}
