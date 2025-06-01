65a5d4f2ea581096464a241f465b69fcf502cfc3
旺看天 <2417266532@qq.com>
2025-06-01 22:35:31 +0800

    fix: git命令执行失败 debug

--- review-sdk/src/main/java/com/grin/Main.java
+++ review-sdk/src/main/java/com/grin/Main.java
@@ -52,13 +52,13 @@ public class Main {
         }
         System.out.println("githubToken:"+githubToken);
         // 获取需要评审的代码
-        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "Head^", "Head");
-        // 设置命令执行目录，防止权限不足
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD^", "HEAD");
+        // 设置命令执行目录
         processBuilder.directory(new File("."));
         Process process = processBuilder.start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
-
+        // 获取代码变更
         StringBuilder diffCode = new StringBuilder();
         while ((line = reader.readLine()) != null) {
             diffCode.append(line);
@@ -66,7 +66,7 @@ public class Main {
         // 读取错误输出（关键！）
         BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
         while ((line = stderr.readLine()) != null) {
-            System.err.println("Git Error: " + line); // 打印错误信息
+            System.err.println("Git Error: " + line);
         }
 
         System.out.println("diffCode：\n" + diffCode);
